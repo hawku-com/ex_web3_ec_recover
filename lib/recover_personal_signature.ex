ea1aea60d85e12c41ae2132ffb676cf3c25d29d6
JJ <jakub@confly.pl>
2022-01-07 23:16:18 +0100

    Basic data encoding

--- lib/recover_personal_signature.ex
+++ lib/recover_personal_signature.ex
@@ -1,17 +1,19 @@
 defmodule ExWeb3EcRecover.RecoverPersonalSignature do
-  @moduledoc """
-  Documentation for `RecoverPersonalSignature`.
-  """
+  @moduledoc false
 
   def recover_personal_signature(%{sig: signature_hex, msg: message}) do
     {r, s, v_num} = convert_sig_to_components(signature_hex)
-    bin_message = create_hash_from_personal_message(message)
-    {:ok, recovered_key} = ExSecp256k1.recover(bin_message, r, s, v_num)
 
-    get_address_from_recovered_key(recovered_key)
+    message
+    |> create_hash_from_personal_message()
+    |> ExSecp256k1.recover(r, s, v_num)
+    |> case do
+      {:ok, recovered_key} -> get_address_from_recovered_key(recovered_key)
+      {:error, _reason} = error -> error
+    end
   end
 
-  def get_address_from_recovered_key(key) do
+  defp get_address_from_recovered_key(key) do
     address =
       key
       |> binary_part(1, 64)
@@ -22,9 +24,10 @@ defmodule ExWeb3EcRecover.RecoverPersonalSignature do
     "0x#{address}"
   end
 
-  def convert_sig_to_components(signature_hex) do
+  defp convert_sig_to_components(signature_hex) do
     sig_binary =
-      String.trim_leading(signature_hex, "0x")
+      signature_hex
+      |> String.trim_leading("0x")
       |> Base.decode16!(case: :lower)
 
     r = binary_part(sig_binary, 0, 32)
@@ -37,15 +40,8 @@ defmodule ExWeb3EcRecover.RecoverPersonalSignature do
     {r, s, v_num - 27}
   end
 
-  def create_hash_from_personal_message(orig_message) do
+  defp create_hash_from_personal_message(orig_message) do
     ("\u0019Ethereum Signed Message:\n#{byte_size(orig_message)}" <> orig_message)
     |> ExKeccak.hash_256()
-  end
-
-  def legacyToBuffer(binary) do
-    case Base.decode16(binary) do
-      {:ok, result} -> result
-      _ -> binary
-    end
   end
 end
---
+++ lib/signed_type.ex
@@ -0,0 +1,88 @@
+defmodule ExWeb3EcRecover.SignedTypedData do
+  @allowed_versions [:v3, :v4]
+
+  @max_depth 5
+
+  def recover_signature(data, sig, version) when version in @allowed_versions do
+  end
+
+  def recover_signature(_data, _sig, _version), do: {:error, :unsupported_version}
+
+  def encode(message, types, primary_type, domain \\ %{}) do
+    [
+      encode_types(types, primary_type),
+      encode_type(message, primary_type, types)
+    ]
+    |> :erlang.iolist_to_binary()
+  end
+
+  def encode_type(data, primary_type, spec) do
+    spec[primary_type]
+    |> Enum.map(fn %{"name" => name, "type" => type} ->
+      value = data[name]
+      encode_data(type, value)
+    end)
+    |> Enum.join()
+  end
+
+  defp encode_data(type, value) when type in ["bytes", "string"], do: ExKeccak.hash_256(value)
+
+  defp encode_data("int" <> _bytes_length = type, value),
+    do: ABI.TypeEncoder.encode_raw(value, type)
+
+  defp encode_data("uint" <> _bytes_length = type, value),
+    do: ABI.TypeEncoder.encode_raw(value, type)
+
+  defp encode_data("bytes" <> _bytes_length = type, value),
+    do: ABI.TypeEncoder.encode_raw(value, type)
+
+  defp encode_data(type, value) when type in ["bool", "address"],
+    do: ABI.TypeEncoder.encode_raw(value, type)
+
+  def encode_types(spec, primary_type) do
+    sorted_deps =
+      spec
+      |> find_deps(primary_type)
+      |> MapSet.to_list()
+      |> Enum.sort()
+
+    [primary_type | sorted_deps]
+    |> Enum.map(&format_dep(&1, spec))
+    |> :erlang.iolist_to_binary()
+    |> ExKeccak.hash_256()
+  end
+
+  defp find_deps(spec, primary_types, acc \\ MapSet.new(), depth \\ @max_depth) do
+    spec[primary_types]
+    |> Enum.reduce(acc, fn %{"type" => type}, acc ->
+      if custom_type?(spec, type) do
+        acc = MapSet.put(acc, type)
+        find_deps(spec, type, acc, depth - 1)
+      else
+        acc
+      end
+    end)
+  end
+
+  defp custom_type?(spec, type) do
+    # TODO verify not a builtin type
+    Map.has_key?(spec, type)
+  end
+
+  defp format_dep(dep, spec) do
+    arguments =
+      spec[dep]
+      |> Enum.map(fn %{"name" => name, "type" => type} -> [type, " ", name] end)
+      |> Enum.intersperse(" ")
+
+    [dep, "(", arguments, ")"]
+  end
+
+  # [
+  #   # @output_type_hash,
+  #   ABI.TypeEncoder.encode_raw([output_type], [{:uint, 256}]),
+  #   ABI.TypeEncoder.encode_raw([owner], [{:bytes, 20}]),
+  #   ABI.TypeEncoder.encode_raw([currency], [:address]),
+  #   ABI.TypeEncoder.encode_raw([amount], [{:uint, 256}])
+  # ]
+end
