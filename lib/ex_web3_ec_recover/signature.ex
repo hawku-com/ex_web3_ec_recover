39885d112c4569dfe34010f008e3a9fb84b8c1df
JJ <jakub@confly.pl>
2022-01-08 23:03:27 +0100

    Simplify signature module.

--- lib/ex_web3_ec_recover/signature.ex
+++ lib/ex_web3_ec_recover/signature.ex
@@ -3,18 +3,8 @@ defmodule ExWeb3EcRecover.Signature do
   defstruct @enforce_keys
 
   def from_binary(signature) do
-    {r, s, v_num} = convert_sig_to_components(signature)
-
-    %__MODULE__{
-      r: r,
-      s: s,
-      v_num: v_num
-    }
-  end
-
-  defp convert_sig_to_components(signature_hex) do
     sig_binary =
-      signature_hex
+      signature
       |> String.trim_leading("0x")
       |> Base.decode16!(case: :lower)
 
@@ -25,6 +15,10 @@ defmodule ExWeb3EcRecover.Signature do
       binary_part(sig_binary, 64, 1)
       |> :binary.decode_unsigned()
 
-    {r, s, v_num - 27}
+    %__MODULE__{
+      r: r,
+      s: s,
+      v_num: v_num - 27
+    }
   end
 end
